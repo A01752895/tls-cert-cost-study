11e3f9fa59961c6a66b0afcbd7f6983f61d472c4
A01752895 <149398770+A01752895@users.noreply.github.com>
2026-04-19 20:24:55 -0600

    Update run_server.sh

--- tls_server/run_server.sh
+++ tls_server/run_server.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+set -e
+
 CERT_DIR=$1
 PORT=$2
 PROTO=$3
@@ -7,4 +9,9 @@ FLAG=""
 [ "$PROTO" = "tls1_2" ] && FLAG="-tls1_2"
 [ "$PROTO" = "tls1_3" ] && FLAG="-tls1_3"
 
-openssl s_server -accept "$PORT" -cert "$CERT_DIR/leaf.crt" -key "$CERT_DIR/leaf.key" $FLAG -www
+openssl s_server \
+  -accept "$PORT" \
+  -cert "$CERT_DIR/leaf.crt" \
+  -key "$CERT_DIR/leaf.key" \
+  -cert_chain "$CERT_DIR/chain.pem" \
+  $FLAG -www

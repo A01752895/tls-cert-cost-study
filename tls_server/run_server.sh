f5ba02ee6450a86492d4931fe598347a034c4bf7
A01752895 <149398770+A01752895@users.noreply.github.com>
2026-04-19 20:09:18 -0600

    Create run_server.sh

---
+++ tls_server/run_server.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+CERT_DIR=$1
+PORT=$2
+PROTO=$3
+
+FLAG=""
+[ "$PROTO" = "tls1_2" ] && FLAG="-tls1_2"
+[ "$PROTO" = "tls1_3" ] && FLAG="-tls1_3"
+
+openssl s_server -accept "$PORT" -cert "$CERT_DIR/leaf.crt" -key "$CERT_DIR/leaf.key" $FLAG -www
